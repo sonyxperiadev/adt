ac65da673db98627934d9acf411b9b1d02693707
synesenom <enys.mones@gmail.com>
2018-02-10 15:06:53 +0100

    some more modules added to index.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 var data = require("./src/data");
+var rest = require("./src/rest");
 var signals = require("./src/signals");
+var systemLog = require("./src/system.log");
+var systemVersion = require("./src/system.version");
+var user = require("./src/user");
+var AreaChart = require("./src/widgets.areachart");
+var BarChart = require("./src/widgets.barchart");
+var ChordChart = require("./src/widgets.chordchart");
+var Grid = require("./src/widgets.grid");
+var Hint = require("./src/widgets.hint");
+var Image = require("./src/widgets.image");
 
 module.exports = {
     data: data,
-    signals: signals
+    rest: rest,
+    signals: signals,
+    system: {
+        log: systemLog,
+        version: systemVersion
+    },
+    user: user,
+    widgets: {
+        AreaChart: AreaChart,
+        BarChart: BarChart,
+        ChordChart: ChordChart,
+        Grid: Grid,
+        Hint: Hint,
+        Image: Image
+    }
 };
